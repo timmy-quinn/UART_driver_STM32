e2a931e6de0eaa2f834916d43e2ac21110fcc2c5
timmy-quinn <tquinn5301@gmail.com>
2024-08-22 23:19:02 -0400

    added basic blink test to main.c

--- app/main.c
+++ app/main.c
@@ -11,17 +11,27 @@
 // Two 32 bit alternate function selection registers GPIOx_AFRH and GPIOx_AFRL 
 
 void setLED(){
-
     // SET GPIOAEN to 1
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     // Set GPIOA_MODER to output
-    GPIOA->MODER
+    GPIOA->MODER |= GPIO_MODER_MODER5; // Set the gpio pin to output
+    GPIOA->OTYPER |= GPIO_OTYPER_OT_5; // Set the gpio pin output to high
+}
+
+void toggle_LED() {
+    GPIOA->ODR ^= GPIO_ODR_ODR_5;
 }
 
 int main(void) {
-
+    uint16_t timer = 0;
+    setLED();
     while(1){
         // set GPIOA_ODR to output
-        
+        if(timer == 10000)
+        {
+            toggle_LED();
+            i = 0;
+        }
+        i++; 
     }
 }
