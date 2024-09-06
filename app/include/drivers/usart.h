6dabb579b8b4c7a56f502de8e12a4e809424a3f3
Timmy-Quinn <timmy.quinn@hannainst.com>
2024-09-05 21:36:17 -0400

    fixed with USART type definition

--- app/include/drivers/usart.h
+++ app/include/drivers/usart.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
 void USART_enable();
-void USART_transmit_byte(uint8_t* data, uint8_t length); 
+void USART_transmit_byte(uint8_t* data, uint8_t length, USART_TypeDef* USART); 
